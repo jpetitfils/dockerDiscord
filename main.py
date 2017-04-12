ec6f9ad854380362dcbe0bb04fb1a80bbd2c5ad0
root <root@Debian>
2017-04-12 10:39:35 +0200

    test

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages. and dan is a pute'.format(counter))
+    if message.content.startswith('dan'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages. and dan is a pute'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run('MzAxMjc2NDE1MjY0MDMwNzIw.C8430g.OE0ExXzSxJlHd4ti0UWFOgPni_4')
